0031ceb2e0b1a9817a3ba18bdd3890a11ac83d2b
kevin.montrose <kevin.montrose@6c66e138-e654-0410-9560-9b6f9fbe9ba3>
2008-08-26 19:12:38 +0000

    Changing license header on every source file.

--- sexpression/parser/UnexpectedTokenException.java
+++ sexpression/parser/UnexpectedTokenException.java
@@ -2,18 +2,24 @@
   * This file is part of VoteBox.
   * 
   * VoteBox is free software: you can redistribute it and/or modify
-  * it under the terms of the GNU General Public License as published by
-  * the Free Software Foundation, either version 3 of the License, or
-  * (at your option) any later version.
-  * 
-  * VoteBox is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  * GNU General Public License for more details.
+  * it under the terms of the GNU General Public License version 3 as published by
+  * the Free Software Foundation.
   * 
   * You should have received a copy of the GNU General Public License
-  * along with VoteBox.  If not, see <http://www.gnu.org/licenses/>.
+  * along with VoteBox, found in the root of any distribution or
+  * repository containing all or part of VoteBox.
+  * 
+  * THIS SOFTWARE IS PROVIDED BY WILLIAM MARSH RICE UNIVERSITY, HOUSTON,
+  * TX AND IS PROVIDED 'AS IS' AND WITHOUT ANY EXPRESS, IMPLIED OR
+  * STATUTORY WARRANTIES, INCLUDING, BUT NOT LIMITED TO, WARRANTIES OF
+  * ACCURACY, COMPLETENESS, AND NONINFRINGEMENT.  THE SOFTWARE USER SHALL
+  * INDEMNIFY, DEFEND AND HOLD HARMLESS RICE UNIVERSITY AND ITS FACULTY,
+  * STAFF AND STUDENTS FROM ANY AND ALL CLAIMS, ACTIONS, DAMAGES, LOSSES,
+  * LIABILITIES, COSTS AND EXPENSES, INCLUDING ATTORNEYS' FEES AND COURT
+  * COSTS, DIRECTLY OR INDIRECTLY ARISING OUR OF OR IN CONNECTION WITH
+  * ACCESS OR USE OF THE SOFTWARE.
  */
+
 package sexpression.parser;
 
 /**
